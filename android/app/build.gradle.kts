d2a7d59b40645758a35e70cd03f002758b3d6ebe
Abdulaziz Malikov <79694711+abdulazizmalikov18@users.noreply.github.com>
2025-12-06 19:50:06 +0500

    update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
         applicationId = "uz.realsoft.carting"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 26
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
